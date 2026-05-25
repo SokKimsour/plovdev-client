4ba7b91122c5b471c9de3c5d7a1cfe3061b30b2d
Genus_ <udomhorn04@gmail.com>
2026-05-25 15:36:26 +0700

    feat: add assets folder and HerosectionCard.jsx and use component in HomePage

--- src/pages/public/HomePage.jsx
+++ src/pages/public/HomePage.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import HeroSectionCard from '../../components/layout/HeroSectionCard'
+import Homeimage from '../../assets/HomepageHeroimage.png'
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className=' m-[auto] p-[auto] mt-28  '>
+     
+     <HeroSectionCard 
+        Title1="Code Smarter Build Real" 
+        Title2 ="Skills." 
+        Subtitle= "Start learning with PlovDev, earn certificates, and land a job." 
+        img={Homeimage} 
+
+        marque1="html"
+        marque2="css"
+        marque3="javascript"
+        marque4="python"
+        marque5="html"
+        marque6="css"
+        
+        />
+       
+    </div>
   )
 }
 
