c553e1fc78836e157fb7fd713c07d4b0e0a0eabd
Genus_ <udomhorn04@gmail.com>
2026-05-25 15:44:25 +0700

    feat: remove data in Homepage

--- src/pages/public/HomePage.jsx
+++ src/pages/public/HomePage.jsx
@@ -1,26 +1,8 @@
 import React from 'react'
-import HeroSectionCard from '../../components/layout/HeroSectionCard'
-import Homeimage from '../../assets/HomepageHeroimage.png'
+
 const HomePage = () => {
   return (
-    <div className=' m-[auto] p-[auto] mt-28  '>
-     
-     <HeroSectionCard 
-        Title1="Code Smarter Build Real" 
-        Title2 ="Skills." 
-        Subtitle= "Start learning with PlovDev, earn certificates, and land a job." 
-        img={Homeimage} 
-
-        marque1="html"
-        marque2="css"
-        marque3="javascript"
-        marque4="python"
-        marque5="html"
-        marque6="css"
-        
-        />
-       
-    </div>
+    <div>HomePage</div>
   )
 }
 
