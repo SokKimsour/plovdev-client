40522bdd2180e8bf623bb8edf213460d31814de1
Genus_ <udomhorn04@gmail.com>
2026-05-25 11:03:07 +0700

    feat: add NavbarHomepage layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import Footer from "./components/layout/Footer"
 
+import { BrowserRouter, Route,Routes } from 'react-router-dom'
+// import GetUser from './components/users/GetUser'
+// import Homepage from './public/Homepage'
+// import Register from './public/Register'
+// import React from 'react'
+// import Coursespage from './public/Coursespage'
+// import AboutUspage from './public/AboutUspage'
+// import Jobboardpage from './public/Jobboardpage'
+// import Userlist from './components/users/Userlist'
+import Navbarhomepage from './components/layout/NavbarHomepage'
+import Footer from './components/layout/Footer'
 function App() {
+
   return (
-    <div>
-      
+    <BrowserRouter>
+      <div className='bg-gray-100'>
+        <div className='w-[85%] xl:w-[90%] max-xl:w-[94%] max-w-[1440px] m-[auto] p-[auto]  justify-center bg-white '>
+        <Navbarhomepage />
+
+ 
+
       <Footer />
       
-    </div>
+      </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
