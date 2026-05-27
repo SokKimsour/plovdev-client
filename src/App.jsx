6c65f4e04c9cb56f4bef9f80dda015177b688b32
Munyvann <kimhak.prome@gmail.com>
2026-05-27 08:15:43 +0700

    feat: remove tag in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,8 @@ import AuthsForm from './components/layout/AuthsForm'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <div className='bg-gray-100'>
-        <div className='w-[85%] xl:w-[90%] max-xl:w-[94%] max-w-[1440px] m-[auto] p-[auto]  justify-center bg-white '>
-        <NavbarBeforeLogin />
-        {/* <NavbarAfterLogin /> */}
-
-    <Routes >
-      <Route path="/" element={<Homepage />} />
-      <Route path="/courses" element={<CoursesPage />} />
-      <Route path="/aboutus" element={<AboutusPage />} />
-      <Route path="/jobboard" element={<JobBoardPage />} />
-    </Routes>
-       
-       <SidebarAdmin />
-       <SidebarUser />
-      <Footer />
-
-
-      </div>
-      </div>
-    </BrowserRouter>
+    <div>
+      
+    </div>
   )
 }
