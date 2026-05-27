0807bfa8bab25a8a8b5b4689ceb5ca7fe9615146
Genus_ <udomhorn04@gmail.com>
2026-05-27 14:22:23 +0700

    feat: change code in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import GetUser from './components/users/GetUser'
+import Homepage from './pages/public/Homepage'
+import CoursesPage from './pages/public/CoursesPage'
+import AboutusPage from './pages/public/AboutusPage'
+import JobBoardPage from './pages/public/JobBoardPage'
+import Footer from './components/layout/Footer'
+import NavbarBeforeLogin from './components/layout/NavbarBeforeLogin'
+import SidebarUser from './components/layout/SidebarUser'
+import SidebarAdmin from './components/layout/SidebarAdmin'
+import NavbarAfterLogin from './components/layout/NavbarAfterLogin'
+import UserDashboard from './route/UserDashboard'
 
-const App = () => {
+function App() {
   return (
-    <div>App</div>
-  )
+      <div className='bg-gray-100'>
+        <div className='max-w-[1440px] m-[auto] p-[auto]  justify-center bg-white '>
+          <NavbarAfterLogin />
+
+          <Routes >
+            <Route path="/" element={<Homepage />} />
+            <Route path="/courses" element={<CoursesPage />} />
+            <Route path="/aboutus" element={<AboutusPage />} />
+            <Route path="/jobboard" element={<JobBoardPage />} />
+            <Route path="/instructor/*" element={<UserDashboard/>}/>
+          </Routes>
+        </div>
+      </div>
+  );
 }
 
-export default App
+export default App;
