66a3f32a77b28c209fb0fc72f6c7c4a329a1e790
Genus_ <udomhorn04@gmail.com>
2026-05-27 14:15:54 +0700

    feat: remvoe data in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,9 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import GetUser from './components/users/GetUser'
-import Homepage from "./pages/public/Homepage";
-import CoursesPage from "./pages/public/CoursesPage";
-import AboutusPage from "./pages/public/AboutusPage";
-import JobBoardPage from "./pages/public/JobBoardPage";
-import Footer from "./components/layout/Footer";
-import NavbarBeforeLogin from "./components/layout/NavbarBeforeLogin";
-import SidebarUser from "./components/layout/SidebarUser";
-import SidebarAdmin from "./components/layout/SidebarAdmin";
-import NavbarAfterLogin from "./components/layout/NavbarAfterLogin";
-import CourseCard from "./components/courses/CourseCard";
-function App() {
-  return (
-      <div className="bg-gray-100">
-        <div className="w-[85%] xl:w-[90%] max-xl:w-[94%] max-w-[1440px] m-[auto] p-[auto]  justify-center bg-white ">
-          <NavbarBeforeLogin />
+import React from 'react'
 
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/courses" element={<CoursesPage />} />
-            <Route path="/aboutus" element={<AboutusPage />} />
-            <Route path="/jobboard" element={<JobBoardPage />} />
-          </Routes>
-            
-          <Footer />
-        </div>
-      </div>
-  );
+const App = () => {
+  return (
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
